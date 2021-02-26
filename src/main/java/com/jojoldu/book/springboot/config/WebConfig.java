fdc823ba8f82a7cf4f284d561b03af91ac59edb2
nam <johndoe@example.com>
2021-02-26 17:48:22 +0900

    cors 해결

--- src/main/java/com/jojoldu/book/springboot/config/WebConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/WebConfig.java
@@ -21,5 +21,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST")
+                .maxAge(3000);
     }
 }
